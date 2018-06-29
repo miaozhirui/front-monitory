6a021e053f37d294fb00ff7882c62b8a9e92d578
Dark <dark.miao@credan.com>
2018-06-29 17:12:31 +0800

    添加时间

--- app/controller/monitor.js
+++ app/controller/monitor.js
@@ -1,12 +1,20 @@
-const BaseController = require('./base')
+const moment = require('moment');
+const BaseController = require('./base') 
 
 module.exports = class MonitorController extends BaseController {
 
     async index() {
 
         const { ctx } = this;
-        let errorInfo = JSON.stringify(ctx.request.body);
+        // let errorInfo = JSON.stringify(ctx.request.body);
+        let errorInfo = ctx.request.body;
+        let time = new Date().getTime();
 
+        errorInfo.time = Math.floor(time/1000);
+        errorInfo.timeFormat = moment(time).format("YYYY-MM-DD hh:mm:ss");
+
+        errorInfo = JSON.stringify(ctx.request.body);
+        
         try{
 
             errorInfo = await ctx.model.Monitor.create({content: errorInfo});
